8e8b719029595f4e76bec355b57f599efa4eea27
mrn <mrn05dev@gmail.com>
2025-09-03 11:40:54 +0200

    fixed needing to manually start treesitter highlighting

--- lua/mrn/plugins/treesitter.lua
+++ lua/mrn/plugins/treesitter.lua
@@ -5,28 +5,57 @@ return {
 		lazy = false,
 		build = ":TSUpdate",
 
-		opts = {
-			-- A list of parser names, or "all"
-			ensure_installed = { "lua", "luadoc", "vim", "vimdoc", "java" },
-
-			-- Install parsers synchronously (only applied to `ensure_installed`)
-			sync_install = false,
-
-			-- Automatically install missing parsers when entering buffer
-			auto_install = true,
-
-			indent = { enable = true },
-
-			highlight = {
-				enable = true,
-
-				-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-				-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-				-- Using this option may slow down your editor, and you may see some duplicate highlights.
-				-- Instead of true it can also be a list of languages
-				additional_vim_regex_highlighting = false,
-			},
+		dependencies = {
+			"nvim-treesitter/playground",
 		},
+
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				-- A list of parser names, or "all"
+				ensure_installed = { "lua", "luadoc", "vim", "vimdoc", "java" },
+
+				-- Install parsers synchronously (only applied to `ensure_installed`)
+				sync_install = false,
+
+				-- Automatically install missing parsers when entering buffer
+				auto_install = true,
+
+				ignore_install = {},
+
+				indent = { enable = true },
+
+				highlight = {
+					enable = true,
+
+					-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+					-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+					-- Using this option may slow down your editor, and you may see some duplicate highlights.
+					-- Instead of true it can also be a list of languages
+					additional_vim_regex_highlighting = false,
+				},
+
+				playground = {
+					enable = true,
+					disable = {},
+					updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+					persist_queries = false, -- Whether the query persists across vim sessions
+					keybindings = {
+						toggle_query_editor = "o",
+						toggle_hl_groups = "i",
+						toggle_injected_languages = "t",
+						toggle_anonymous_nodes = "a",
+						toggle_language_display = "I",
+						focus_language = "f",
+						unfocus_language = "F",
+						update = "R",
+						goto_node = "<cr>",
+						show_help = "?",
+					},
+				},
+
+				modules = {},
+			})
+		end,
 	},
 
 	{
@@ -48,33 +77,5 @@ return {
 			zindex = 20, -- The Z-index of the context window
 			on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
 		},
-	},
-
-	{
-		"nvim-treesitter/playground",
-		after = "nvim-treesitter",
-
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				playground = {
-					enable = true,
-					disable = {},
-					updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-					persist_queries = false, -- Whether the query persists across vim sessions
-					keybindings = {
-						toggle_query_editor = "o",
-						toggle_hl_groups = "i",
-						toggle_injected_languages = "t",
-						toggle_anonymous_nodes = "a",
-						toggle_language_display = "I",
-						focus_language = "f",
-						unfocus_language = "F",
-						update = "R",
-						goto_node = "<cr>",
-						show_help = "?",
-					},
-				},
-			})
-		end,
 	},
 }
