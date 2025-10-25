ae2ace11f93cca3434a42d466d0e49280136a27d
mrn <mrn05dev@gmail.com>
2025-10-25 14:55:57 +0200

    added a shortcut that automatically saves compiles and uploads a sketch onto an arduino

--- lua/mrn/remaps.lua
+++ lua/mrn/remaps.lua
@@ -28,3 +28,5 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 vim.keymap.set("n", "<leader>=", "mygg=G`y<cmd>delmarks y<CR>")
+
+vim.keymap.set("n", "<leader>q", ":<cmd>w | !arduino-cli compile && arduino-cli upload<CR>")
